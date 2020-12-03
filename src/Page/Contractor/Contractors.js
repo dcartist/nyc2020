3480f33c57d3443bc874972b281f3810ac3522de
DCartist <pbannerman13@gmail.com>
2020-12-03 17:40:19 -0500

    add sort by id and last name

--- src/Page/Contractor/Contractors.js
+++ src/Page/Contractor/Contractors.js
@@ -15,14 +15,27 @@ class Contractors extends Component {
             let arrayUniqueByKey = [...new Map(results.data.map(item =>
                 [item["conLicense"], item])).values()];
             this.setState({contractors:arrayUniqueByKey})
-        console.log(results.data)
-        console.log(arrayUniqueByKey)
     })
+    }
+
+    byName =(event) => {
+        event.preventDefault()
+        let changedArr = this.state.contractors.sort((a, b) => (a.conLastName > b.conLastName) ? 1 : -1)
+        this.setState({contractors: changedArr})
+
+    }
+    byId =(event) => {
+        event.preventDefault()
+        let changedArr = this.state.contractors.sort((a, b) => (a.conLicense > b.conLicense) ? 1 : -1)
+        this.setState({contractors: changedArr})
+
     }
 
     render() {
         if (this.state.contractors.length !== 0){
-            return (
+            return (<div>
+                <button onClick={this.byName}>Change to order</button>
+                <button onClick={this.byId}>Change to ID</button>
                 <div className="ContractorListing">
                     {this.state.contractors.map(item=>(<div>
                         <ul>
@@ -30,6 +43,7 @@ class Contractors extends Component {
                     <li>{item.conLicense}</li>
                         </ul>
                     </div>))}
+                </div>
                 </div>
             );
         }else {
