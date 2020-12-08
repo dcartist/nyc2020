527005b1c79a02673a98cd73b280a8bc2f153ed2
DCartist <pbannerman13@gmail.com>
2020-12-08 00:46:35 -0500

    updating home page

--- src/Page/Home/Home.js
+++ src/Page/Home/Home.js
@@ -2,17 +2,20 @@ import React, { Component } from 'react';
 import Image from "../../Component/Images/cetteup-Geh-r9A4RBg-unsplash.jpg"
 import ImageSVG from "./HomeSVG"
 import Animated from "../../Component/Images/construction.svg"
+import { Divider } from 'semantic-ui-react'
+
 class Home extends Component {
     render() {
         return (
             <div className="HomeBase">
-                <section>
+                <section className="HomeLeftSide">
                     <h1>New York City</h1>
                     <h3>Contractor and Jobs Listings</h3>
-                    <p></p>
+                    <hr></hr>
+                    <p>Construction job applications submitted through the Borough Offices, through eFiling, or through the HUB from 2000 to 2019.</p>
                 </section>
                 <section>
-                    <img src={Animated} width="90%" height="auto"/>
+                    <img src={Animated} width="85%" height="auto" alt="Animated SVG of contruction"/>
                 </section>
             </div>
         );
